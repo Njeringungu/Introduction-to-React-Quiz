cf1d2e41ff58fed73e1125a9e10999712fcce77c
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-07-18 17:56:44 +0300

    done 1and2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +11,12 @@ const users = [
   { name: "Jane Doe", id: 2 },
   { name: "Billy Doe", id: 3 }
 ];
+const userList = users.map((user) => {
+  return <li key={user.id}> {user.name}</li>}
+  );
+  
+
+ 
 
 // comment this out after completion and uncomment code below it to proceed
 function Child() {
@@ -30,7 +36,8 @@ function Child() {
 
 function Parent() {
   const [value, setValue] = React.useState(
-    "I need to be updated from my child"
+    const [value,setValue]=useState( "I need to be updated from my child")
+   
   );
 
   return (
@@ -62,16 +69,27 @@ function Parent() {
 
 function App() {
   const [] = React.useState(true);
+  
+  const [hideToggle,setHideToggle]=useState(false);
+
+function handleClick() {
+  setHideToggle(hideToggle ? null : "Toggle Challenge")
+  
+}
   return (
     
     <>
     
     <img src={logo} className="App-logo" alt="logo" />
-      <h3>User names</h3>
-      <ul></ul>
-      <button>Hide Element Below</button>
+    <h1 style={{color : "blue"}}>JSX is cool!</h1>
+      <h3 style={{color : "blue"}}>User names</h3>
+      <ul>{userList}</ul>
+      <button onClick={handleClick}>Hide Element Below</button>
 
-      <div>Toggle Challenge</div>
+      <div >Toggle Challenge</div>
+
+     
+      
       <Parent>
       <Child />
     </Parent>
